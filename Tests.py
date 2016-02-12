59fa24dee987dd3420b3ee5092621813a2de126b
Mirza <mkuljancic@student.tgm.ac.at>
2016-02-12 12:12:04 +0100

    Alle Testfaelle_angepasst

--- Tests.py
+++ Tests.py
@@ -8,16 +8,16 @@ def test_eingabe_sollte_erfolgreich_sein():
 
 def test_eingabe_bindestrich_fehler_im_dialekt():
     eingabe("infile1.csv","infile2_bindestrich.csv")
-    assert_true(filecmp.cmp('outfile_test_eingabe_bindestrich_fehler_im_dialekt_1_2_fehlerhaft.csv','outfile.csv'))
+    assert_true(filecmp.cmp('outfile_test_eingabe_bindestrich_fehler_im_dialekt.csv','outfile.csv'))
 
 def test_eingabe_punkt_fehler_im_dialekt():
     eingabe("infile1.csv","infile2_feherhaft_punkt.csv")
-    assert_true(filecmp.cmp('outfile_test_eingabe_punkt_fehler_im_dialekt_1_2_fehlerhaft.csv','outfile.csv'))
+    assert_true(filecmp.cmp('outfile_fehler_punkt.csv','outfile.csv'))
 
 def test_eingabe_buchstabe_fehler_im_dialekt():
     eingabe("infile1.csv","infile2_buchstabe.csv")
-    assert_true(filecmp.cmp('outfile_test_eingabe_buchstabe_fehler_im_dialekt_1_2_fehlerhaft.csv','outfile.csv'))
+    assert_true(filecmp.cmp('outfile_buchstabe.csv','outfile.csv'))
 
 def test_eingabe_leer():
     eingabe("infile1.csv","infile2_leer.csv")
-    assert_true(filecmp.cmp('test_eingabe_leer_1_2_fehlerhaft.csv','outfile.csv'))
+    assert_true(filecmp.cmp('outfile_leer.csv','outfile.csv'))
