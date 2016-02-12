4b5ccc662f58bd261d6cdec68010bf08b4a6fd83
Mirza <mkuljancic@student.tgm.ac.at>
2016-02-12 12:41:06 +0100

    Alle Testfaelle_angepasst_IOError

--- Tests.py
+++ Tests.py
@@ -1,23 +1,21 @@
-__author__ = 'Kuljancic'
 import filecmp
 from csvtest import eingabe
-from nose.tools import assert_true
+from nose.tools import assert_false,assert_raises
 def test_eingabe_sollte_erfolgreich_sein():
-    eingabe("infile1.csv","infile2.csv")
-    assert_true(filecmp.cmp('outfile_test_eingabe_erfolgreich_1_2.csv','outfile.csv'))
+    assert_raises(IOError, eingabe("xsxs.csv","rrfr.csv"))
 
 def test_eingabe_bindestrich_fehler_im_dialekt():
     eingabe("infile1.csv","infile2_bindestrich.csv")
-    assert_true(filecmp.cmp('outfile_test_eingabe_bindestrich_fehler_im_dialekt.csv','outfile.csv'))
+    assert_false(filecmp.cmp('outfile_test_eingabe_erfolgreich_1_2.csv','outfile.csv'))
 
 def test_eingabe_punkt_fehler_im_dialekt():
     eingabe("infile1.csv","infile2_fehlerhaft_punkt.csv")
-    assert_true(filecmp.cmp('outfile_fehler_punkt.csv','outfile.csv'))
+    assert_false(filecmp.cmp('outfile_test_eingabe_erfolgreich_1_2.csv','outfile.csv'))
 
 def test_eingabe_buchstabe_fehler_im_dialekt():
     eingabe("infile1.csv","infile2_buchstabe.csv")
-    assert_true(filecmp.cmp('outfile_buchstabe.csv','outfile.csv'))
+    assert_false(filecmp.cmp('outfile_test_eingabe_erfolgreich_1_2.csv','outfile.csv'))
 
 def test_eingabe_leer():
     eingabe("infile1.csv","infile2_leer.csv")
-    assert_true(filecmp.cmp('outfile_leer.csv','outfile.csv'))
+    assert_false(filecmp.cmp('outfile_test_eingabe_erfolgreich_1_2.csv','outfile.csv'))
