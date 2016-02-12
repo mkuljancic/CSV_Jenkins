9f914fc27820ef94d755d586171c5ec7c1f0b50b
Mirza <mkuljancic@student.tgm.ac.at>
2016-02-12 11:38:37 +0100

    Fehler im ersten Testfall entfernt

--- Tests.py
+++ Tests.py
@@ -3,5 +3,5 @@ import filecmp
 from csvtest import eingabe
 def test_eingabe_sollte_erfolgreich_sein():
     eingabe("infile1.csv","infile2.csv")
-    assert filecmp.cmp('outfile_test_eingabe_erfolgreich.csv','outfile.csv')
+    assert filecmp.cmp('outfile_test_eingabe_erfolgreich_1_2.csv','outfile.csv')
 
